68b5e5b0c121d5f251b8b12630e0cde5f0b5d8f3
adudley78 <dudleyadam@gmail.com>
2019-07-01 10:58:31 -0400

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,21 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const { reviews, deleteReviews } = this.props;
+    const reviewList = reviews.map(reviews => {
+      return (
+  <Review
+          key={review.id}
+          review={review}
+          deleteReview={deleteReview}
+        />
+      )
+    });
+    
     return (
       <ul>
         Reviews
+        { reviewList }
       </ul>
     );
   }
